598dd5c25de88527918d6638327029eff295557d
wusaYa <wusaYa@outlook>
2018-01-24 15:05:25 +0800

    pod once

---
+++ WSTipsView/WSTipsView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WSTipsView
+//
+//  Created by 吴洒 on 2018/1/24.
+//  Copyright © 2018年 吴洒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WSTipsView/WSTipsView/WSCustomView.m
@@ -0,0 +1,21 @@
+//
+//  WSCustomView.m
+//  WSTipsView
+//
+//  Created by 吴洒 on 2018/1/24.
+//  Copyright © 2018年 吴洒. All rights reserved.
+//
+
+#import "WSCustomView.h"
+
+@implementation WSCustomView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
