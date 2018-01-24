52e5f478ed6c53852111eefd5c999f7ad65056b2
wusaYa <wusaYa@outlook>
2018-01-24 16:10:55 +0800

    0.0.2测试提交

---
+++ WSTipsView/WSTipsView/WSTextViews.h
@@ -0,0 +1,13 @@
+//
+//  WSTextViews.h
+//  WSTipsView
+//
+//  Created by 吴洒 on 2018/1/24.
+//  Copyright © 2018年 吴洒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WSTextViews : UIView
+
+@end
---
+++ WSTipsView/WSTipsView/WSTextViews.m
@@ -0,0 +1,21 @@
+//
+//  WSTextViews.m
+//  WSTipsView
+//
+//  Created by 吴洒 on 2018/1/24.
+//  Copyright © 2018年 吴洒. All rights reserved.
+//
+
+#import "WSTextViews.h"
+
+@implementation WSTextViews
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
